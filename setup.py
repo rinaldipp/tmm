711df574eba0e9c793127c837ddde1e8615c452e
rinaldipp <rinaldipp@gmail.com>
2021-05-12 16:37:41 -0300

    Reestructuring project tree.

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 from glob import glob
+import setuptools
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-settings = {
-    'name': 'tmm',
-    'version': '0.0.1',
-    'description': 'TMM for modeling acoustic treatments',
-    'long_description': long_description,
-    'long_description_content_type': 'text/markdown',
-    'url': 'https://github.com/rinaldipp/tmm',
-    'author': 'Rinaldi Polese Petrolli',
-    'author_email': 'rinaldipp@gmail.com',
-    'license': 'MIT',
-    'install_requires': ['numpy', 'scipy', 'matplotlib',
-        'pandas', 'mpmath', 'xlsxwriter'],
-    'classifiers': [
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+setuptools.setup(
+    name="tmm",
+    version="0.0.1",
+    author="Rinaldi Polese Petrolli",
+    author_email="rinaldipp@gmail.com",
+    description="TMM for modeling acoustic treatments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rinaldipp/tmm",
+    packages=setuptools.find_packages(),
+    install_requires = ['numpy', 'scipy', 'matplotlib', 'pandas', 'mpmath', 'xlsxwriter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent", ],
-    'python_requires': '>=3.6, <3.9',
-}
-
-setup(**settings)
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
