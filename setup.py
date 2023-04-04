a3b45730de2e3139288f9110bf314d80caf3c761
rinaldipp <rinaldipp@gmail.com>
2023-04-04 14:08:28 -0300

    [FIX] Added MANIFEST.in file to include .csv files during pip installation.

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="tmm",
-    version="0.0.2",
+    version="0.0.3",
     author="Rinaldi Polese Petrolli",
     author_email="rinaldipp@gmail.com",
-    description="TMM for modeling acoustic treatments",
+    description="Transfer Matrix Models for modeling acoustic treatments",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/rinaldipp/tmm",
     packages=setuptools.find_packages(),
-    install_requires = ['numpy', 'scipy', 'matplotlib', 'pandas', 'mpmath', 'xlsxwriter', 'h5py'],
+    install_requires=['numpy', 'scipy', 'matplotlib', 'pandas', 'mpmath', 'xlsxwriter', 'h5py'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
